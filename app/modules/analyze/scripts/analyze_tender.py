df69d8a0a8a9de5a53b91881a9809b325ab6d601
WinterSunset95 <wintersunset95@gmail.com>
2025-11-10 18:56:49 +0530

    feat: added the new analyze_tender.py file and documentation

--- app/modules/analyze/scripts/analyze_tender.py
+++ app/modules/analyze/scripts/analyze_tender.py
@@ -1,3 +1,10 @@
 
-def analyze_tender(tdr: str):
+from sqlalchemy.orm.session import Session
+from app.db.database import SessionLocal
+
+def analyze_tender(db: Session, tdr: str):
+
+    pass
+
+def get_wishlisted_tenders(db: Session, tdr: str):
     pass
